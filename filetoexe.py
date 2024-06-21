93dd4ff0d0a967348c721f15cae68c5212816ad5
Vikrant Kadam <139327456+VikrantKadam028@users.noreply.github.com>
2024-06-21 10:10:42 +0530

    Add files via upload

---
+++ filetoexe.py
@@ -0,0 +1,14 @@
+#-------1st Method--------------------------------------
+
+# To do this direct in cmd - > 
+#1.put pyinstaller -filename-.py
+#2.it get converted to EXE file
+
+#But drawback is that it convert and create multiple file!!
+
+# To create only a single file ->
+#1.type pyinstaller --onefile -filename-.py
+
+#-------2st Method--------------------------------------
+#by using auto-py-to-exe
+#1.put auto-py-to-exe in cmd and enter
